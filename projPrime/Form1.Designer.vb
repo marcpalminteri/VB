6195fadb32c9e13c514d5a86ab4272d46f3f95ea
unknown <marcpalminteri@gmail.com>
2015-09-10 11:41:59 +0100

    Prime number project

---
+++ projPrime/Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPrime
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtMin = New System.Windows.Forms.TextBox()
+        Me.txtMax = New System.Windows.Forms.TextBox()
+        Me.txtDisplay = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnReport = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblMin = New System.Windows.Forms.Label()
+        Me.lblMax = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtMin
+        '
+        Me.txtMin.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtMin.Location = New System.Drawing.Point(147, 13)
+        Me.txtMin.Name = "txtMin"
+        Me.txtMin.Size = New System.Drawing.Size(95, 26)
+        Me.txtMin.TabIndex = 0
+        '
+        'txtMax
+        '
+        Me.txtMax.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtMax.Location = New System.Drawing.Point(147, 45)
+        Me.txtMax.Name = "txtMax"
+        Me.txtMax.Size = New System.Drawing.Size(95, 26)
+        Me.txtMax.TabIndex = 1
+        '
+        'txtDisplay
+        '
+        Me.txtDisplay.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDisplay.Location = New System.Drawing.Point(19, 77)
+        Me.txtDisplay.Multiline = True
+        Me.txtDisplay.Name = "txtDisplay"
+        Me.txtDisplay.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtDisplay.Size = New System.Drawing.Size(223, 159)
+        Me.txtDisplay.TabIndex = 2
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(248, 13)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(107, 61)
+        Me.btnCalculate.TabIndex = 3
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnReport
+        '
+        Me.btnReport.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReport.Location = New System.Drawing.Point(248, 94)
+        Me.btnReport.Name = "btnReport"
+        Me.btnReport.Size = New System.Drawing.Size(107, 61)
+        Me.btnReport.TabIndex = 4
+        Me.btnReport.Text = "Report"
+        Me.btnReport.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(248, 175)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(107, 61)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblMin
+        '
+        Me.lblMin.AutoSize = True
+        Me.lblMin.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMin.ForeColor = System.Drawing.Color.White
+        Me.lblMin.Location = New System.Drawing.Point(15, 16)
+        Me.lblMin.Name = "lblMin"
+        Me.lblMin.Size = New System.Drawing.Size(126, 19)
+        Me.lblMin.TabIndex = 6
+        Me.lblMin.Text = "Minimum Value"
+        '
+        'lblMax
+        '
+        Me.lblMax.AutoSize = True
+        Me.lblMax.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMax.ForeColor = System.Drawing.Color.White
+        Me.lblMax.Location = New System.Drawing.Point(15, 48)
+        Me.lblMax.Name = "lblMax"
+        Me.lblMax.Size = New System.Drawing.Size(126, 19)
+        Me.lblMax.TabIndex = 7
+        Me.lblMax.Text = "Maximum Value"
+        '
+        'frmPrime
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.SeaGreen
+        Me.ClientSize = New System.Drawing.Size(367, 248)
+        Me.Controls.Add(Me.lblMax)
+        Me.Controls.Add(Me.lblMin)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReport)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtDisplay)
+        Me.Controls.Add(Me.txtMax)
+        Me.Controls.Add(Me.txtMin)
+        Me.Name = "frmPrime"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Prime Numbers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtMin As System.Windows.Forms.TextBox
+    Friend WithEvents txtMax As System.Windows.Forms.TextBox
+    Friend WithEvents txtDisplay As System.Windows.Forms.TextBox
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents btnReport As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents lblMin As System.Windows.Forms.Label
+    Friend WithEvents lblMax As System.Windows.Forms.Label
+
+End Class
---
+++ projPrime/Form1.vb
@@ -0,0 +1,82 @@
+﻿Imports System.IO
+Imports System.Math
+
+Public Class frmPrime
+
+    Private Function CheckInput()
+        If txtMin.Text = "" Or txtMax.Text = "" Then
+            MessageBox.Show("Enter numbers into both fields")
+            Return False
+        End If
+        If Not IsNumeric(txtMin.Text) Or Not IsNumeric(txtMax.Text) Then
+            MessageBox.Show("Only enter numbers into the fields")
+            Return False
+        End If
+        If txtMin.Text < 1 Then
+            MessageBox.Show("Enter a larger number than 0 in the minimum field")
+            Return False
+        End If
+        If Integer.Parse(txtMin.Text) >= Integer.Parse(txtMax.Text) Then
+            MessageBox.Show("Make sure the Minimum is less than the Maximum")
+            Return False
+        End If
+        Return True
+    End Function
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        If CheckInput() Then
+            Dim min As Integer = Integer.Parse(txtMin.Text)
+            Dim max As Integer = Integer.Parse(txtMax.Text)
+            Dim prime As Boolean
+            Dim stringOfPrime As String = ""
+            txtDisplay.Clear()
+
+            If min = 1 Or min = 2 Then
+                stringOfPrime = "2 "
+            End If
+
+            If min Mod 2 = 0 Then
+                min = min + 1
+            End If
+
+            If min = 1 Then
+                min = min + 2
+            End If
+
+            For current As Integer = min To max Step 2
+                prime = True
+                For i As Integer = 2 To Sqrt(current)
+                    If current Mod i = 0 Then
+                        prime = False
+                        Exit For
+                    End If
+                Next
+                If prime Then
+                    stringOfPrime = stringOfPrime & current & " "
+                End If
+            Next
+            txtDisplay.Text = stringOfPrime.Substring(0, stringOfPrime.Length - 1)
+        End If
+    End Sub
+
+    Private Sub btnReport_Click(sender As Object, e As EventArgs) Handles btnReport.Click
+        If txtDisplay.Text <> "" Then
+            Dim fileLoc As String = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+            fileLoc = fileLoc & "\Test.txt"
+            If File.Exists(fileLoc) Then
+                File.Delete(fileLoc)
+            End If
+
+            Dim sw As StreamWriter = New StreamWriter(fileLoc)
+            sw.Write(txtDisplay.Text)
+            sw.Close()
+        Else
+            MessageBox.Show("Make sure there is a calculation to report")
+            Return
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
